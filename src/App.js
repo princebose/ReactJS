96ddecec0376fcfee51931fa890c7216af47a8b4
princebose <boseprince11@gmail.com>
2020-07-09 14:56:12 -0400

    5-props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
+import A from './components/Greet'
 // function App() {
 //   return (
 //     <div className="App">
@@ -12,13 +12,17 @@ import Greet from './components/Greet'
 //   );
 // }
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <div className="App">
-      <h1>Hello World</h1>
-      <Greet></Greet>
-    </div>
+        <h1>Hello World</h1>
+        <A heroName="Superman" name="Clark">
+          <p>This is children props</p></A>
+        <A heroName="Batman" name="Bruce"></A>
+        <A heroName="Spiderman" name="Peter"></A>
+        <A heroName="He-Man" name="Prince"></A>
+      </div>
     );
   }
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,20 +1,22 @@
 import React from 'react'
 
-// function Greet(){
-//     return(
-//         <div className="App">
-//         <h1>Hello from Greet</h1>
-//         </div>
-//     )
-// }
-
-const Greet = ()=>{
-    return(
+function Greet(props) {
+    //return React.createElement('div',null,React.createElement('h1',null,'Greet Compnent'))
+    return (
         <div className="App">
-        <h1>Hello from Greet Component using arrow fn.</h1>
+            <h1 > Hello {props.name} a.k.a {props.heroName} from Greet</h1>
+            <h3 >{props.children}</h3>
         </div>
     )
+} //JSX - Javascript and HTML combined, JS compiles HTML as well
 
-}
+// const Greet = ()=>{
+//     return(
+//         <div className="App">
+//         <h1>Hello from Greet Component using arrow fn.</h1>
+//         </div>
+//     )
+
+// }
 
 export default Greet;
