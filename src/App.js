6824a8ae96797305eab4f2ba87e43af88278f761
princebose <boseprince11@gmail.com>
2020-07-09 14:18:34 -0400

    2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         Hello World
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
       <header className="App-header">
-        Hello World
+        Hello World Component
       </header>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
